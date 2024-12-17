d765e6169dd4142a37a8e8c416a361bb23ef5d5e
yxc08 <166981714+yxc08@users.noreply.github.com>
2024-12-17 15:36:53 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -1,7 +1,7 @@
 
 import { connect } from 'cloudflare:sockets';
 
-let userID = '';
+let userID = '9b558d3f-caec-4943-911a-4b8b38d7e106';
 let proxyIP = '';
 let sub = '';
 let subConverter = 'SUBAPI.fxxk.dedyn.io';
